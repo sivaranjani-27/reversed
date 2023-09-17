5c65b94e8a62c3f2baee78ccb891f99690e0d6bb
sivaranjani-27 <ranjanikarthi846@gmail.com>
2023-09-17 12:03:09 +0530

    first commit

---
+++ OneDrive/Documents/NetBeansProjects/array/src/array/Array.java
@@ -0,0 +1,25 @@
+package array;
+public class Array {
+        public static void main(String[]args) {
+        int[] nums={2,3,4,5};
+        System.out.println(nums);
+        System.out.println(nums[0]);
+       String word="HELLOWORLD";
+       System.out.println("\"HELLOWORLD\"");
+       String[] names={"mom","dad","sis"};
+       System.out.println(names[0]);
+       float[] b={23.2f,89.3f};
+       System.out.println(b[1]);
+       double[]c={22.5,77.5,89.7};
+       System.out.println(c[2]);
+       long[] d= {12345,67908,59876};
+       System.out.println(d[0]);
+       short[] e={1234,5687,6987};
+       System.out.println(e[0]);
+       char[] f={'r','g','b','d'};
+       System.out.println(f[3]);
+       int []g={123456,567876,989898};
+       System.out.println(g[0]);
+        
+        }
+        }
