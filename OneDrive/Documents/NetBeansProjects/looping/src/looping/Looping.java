7a958b2790a188b6d13ebccbf186b4318b69829a
sivaranjani-27 <ranjanikarthi846@gmail.com>
2023-09-25 21:49:28 +0530

    looping

---
+++ OneDrive/Documents/NetBeansProjects/looping/src/looping/Looping.java
@@ -0,0 +1,34 @@
+package looping;
+
+public class Looping {
+
+    public static void main(String[] args) {
+        int i=5;
+        while(i<=100)          
+        {
+            if(i>=20&&i<=35)
+            {
+                i+=5;
+            continue;
+        }
+            System.out.println(i);
+            if(i==75)
+            {
+                break;
+            }
+            i+=5;
+            
+        }
+    }
+}
+        
+
+     
+        
+
+        
+        
+                
+        
+                
+    
