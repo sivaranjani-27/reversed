b893bd7a757562a73ebf6307ace922140f240147
sivaranjani-27 <ranjanikarthi846@gmail.com>
2023-10-02 22:42:03 +0530

    convertion

---
+++ OneDrive/Documents/NetBeansProjects/convertion/src/convertion/Convertion.java
@@ -0,0 +1,13 @@
+package convertion;
+
+import java.util.Scanner;
+
+public class Convertion {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m=1,mm=1000,cm=100;
+        System.out.println("");    
+    }
+
+}
