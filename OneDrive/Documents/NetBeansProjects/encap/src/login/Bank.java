bbcac0c58a31d7e3a2243ba4b58902574b684bce
sivaranjani-27 <ranjanikarthi846@gmail.com>
2023-11-08 19:44:11 +0530

    encap

---
+++ OneDrive/Documents/NetBeansProjects/encap/src/login/Bank.java
@@ -0,0 +1,34 @@
+
+package login;
+public class Bank {
+private int ac;
+private int amt;
+private String name;
+
+    public int getAmt() {
+        return amt;
+    }
+
+    public void setAmt(int amt) {
+        this.amt = amt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAc() {
+        return ac;
+    }
+
+    public void setAc(int ac) {
+        this.ac = ac;
+    }
+
+    }
+
+        
---
+++ OneDrive/Documents/NetBeansProjects/encap/src/login/Demo.java
@@ -0,0 +1,21 @@
+
+package login;
+
+
+public class Demo {
+    public static void main(String args[]){
+        Bank c1=new Bank();
+        c1.setAc(100);
+        System.out.println(c1.getAc());
+         Bank c2=new Bank();
+        c2.setAmt(10000);
+        System.out.println(c2.getAmt());
+         Bank c3=new Bank();
+        c3.setName("Ranju");
+        System.out.println(c1.getName());
+
+
+        
+    }
+    
+}
